b065891595310baea2663be7d2c1c1cc20b55ad9
nightprayer90 <m_steguweit@mail.de>
2023-05-21 11:26:42 +0200

    menu navigation

---
+++ GDTVJAM2023/Assets/Scripts/MenuButtonController.cs
@@ -0,0 +1,34 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonController : MonoBehaviour
+{
+    public string gameScene = "GameScene";
+
+    //Main Menu
+    public void GameStart()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("CreditScene");
+    }
+
+    public void GameQuit()
+    {
+        Application.Quit();
+    }
+
+    //Cedit Scene
+    public void BacktoMainMenue()
+    {
+        SceneManager.LoadScene("MenueScene");
+    }
+
+    public void LevelRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
