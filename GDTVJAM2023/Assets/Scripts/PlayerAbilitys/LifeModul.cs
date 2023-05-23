5d1ee0b75bacd0eb44513ff2e954cd0007e42a52
nightprayer90 <m_steguweit@mail.de>
2023-05-24 00:10:58 +0200

    Fähigkeiten

---
+++ GDTVJAM2023/Assets/Scripts/PlayerAbilitys/LifeModul.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeModul : MonoBehaviour
+{
+    public List<ParticleSystem> particleSystems;
+    public float tickInterval = 2f;
+    private float nextHealthTick = 12f;
+    public float PercentHealthPerTick = 0.1f;
+    private PlayerController playerController;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+    void Update()
+    {
+        if (playerController.playerCurrentHealth < playerController.playerMaxHealth)
+        {
+            if (Time.time >= nextHealthTick)
+            {
+                foreach (ParticleSystem particleSystem in particleSystems)
+                {
+                    particleSystem.Play();
+                }
+                playerController.UpdatePlayerHealth(-Mathf.Max(1, Mathf.RoundToInt(playerController.playerMaxHealth * PercentHealthPerTick)));
+                nextHealthTick = Time.time + tickInterval;
+            }
+  
+        }
+    }
+}
