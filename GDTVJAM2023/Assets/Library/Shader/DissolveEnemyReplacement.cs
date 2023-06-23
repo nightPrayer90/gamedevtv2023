7c10e24effc5fd35c371b908769e6d12dd239b1a
nightprayer90 <m_steguweit@mail.de>
2023-06-23 20:15:19 +0200

    kram

--- GDTVJAM2023/Assets/Library/Shader/DissolveEnemyReplacement.cs
+++ GDTVJAM2023/Assets/Library/Shader/DissolveEnemyReplacement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DissolveEnemyReplacement : MonoBehaviour
 {
-    public MeshRenderer meshRenderer;
+    public SkinnedMeshRenderer meshRenderer;
 
     private Material[] meshMaterials;
     private float counter;
