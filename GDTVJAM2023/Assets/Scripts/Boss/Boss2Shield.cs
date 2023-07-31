212ca97b1c9a89ac8bbaf3dce55c43d42fe91a45
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2023-08-01 00:17:22 +0200

    work on boss 2

--- GDTVJAM2023/Assets/Scripts/Boss/Boss2Shield.cs
+++ GDTVJAM2023/Assets/Scripts/Boss/Boss2Shield.cs
@@ -8,15 +8,28 @@ public class Boss2Shield : MonoBehaviour
 {
     public float rotationSpeed = 10f;
     public int shieldHealth = 30;
+    public List<ParticleCollisionEvent> collisionEvents; // creating a list to store the collision events
+
+    public ParticleSystem rippleParticle;
+    public ParticleSystem hitParticle;
+
+    public MeshRenderer shieldMesh;
+    private MeshCollider shieldCollider;
+
 
     private void Awake()
     {
-        transform.localScale = new Vector3(1, 1, 1);
+       
     }
 
     private void OnEnable()
     {
+        transform.localScale = new Vector3(1, 1, 1);
+        
+        Debug.Log(shieldCollider);
+
         transform.DOScale(150, 2f);
+        collisionEvents = new List<ParticleCollisionEvent>();
     }
 
     private void Update()
@@ -24,11 +37,38 @@ public class Boss2Shield : MonoBehaviour
         transform.Rotate(new Vector3(0, 0, rotationSpeed) * Time.deltaTime);   
     }
 
+    
     private void OnParticleCollision(GameObject other)
     {
-        Debug.Log(other);
         shieldHealth -= 1;
-        transform.DOComplete();
-        transform.DOPunchScale(new Vector3(0.1f, 0.1f, 0.1f), 0.1f, 10, 1);
+
+        ParticleSystem part = other.GetComponent<ParticleSystem>();
+        int numCollisionEvents = part.GetCollisionEvents(this.gameObject, collisionEvents);
+
+        foreach (ParticleCollisionEvent collisionEvent in collisionEvents) //  for each collision, do the following:
+        {
+            Vector3 pos = collisionEvent.intersection; // the point of intersection between the particle and the enemy
+            hitParticle.transform.position = pos;
+            hitParticle.Emit(30);
+        }
+
+       
+
+        if (shieldHealth <= 0)
+        {
+            rippleParticle.Play();
+            shieldCollider.GetComponent<MeshCollider>();
+            shieldCollider.enabled = false;
+            shieldMesh.enabled = false;
+        }
+        else
+        {
+            transform.DOComplete();
+            transform.DOPunchScale(new Vector3(5f, 5f, 5f), 0.05f, 10, 1));
+            
+        }
+
+       
+
     }
 }
