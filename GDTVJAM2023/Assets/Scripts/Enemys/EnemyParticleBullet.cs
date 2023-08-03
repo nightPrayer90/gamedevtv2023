7246c473717a6097b6ed933ebc2824ad09d69926
Martin Steguweit <mst@igc-online.local>
2023-08-03 15:55:36 +0200

    trying some errors

--- GDTVJAM2023/Assets/Scripts/Enemys/EnemyParticleBullet.cs
+++ GDTVJAM2023/Assets/Scripts/Enemys/EnemyParticleBullet.cs
@@ -5,6 +5,8 @@ public class EnemyParticleBullet : MonoBehaviour
     public int bulletDamage = 0;
     public ParticleSystem bulletParticleSystem;
 
+    public bool canReflect = false;
+    public int maxReflectionCount = 3;
 
     //List<ParticleCollisionEvent> colEvents = new List<ParticleCollisionEvent>();
     public void Start()
@@ -25,5 +27,26 @@ public class EnemyParticleBullet : MonoBehaviour
     {
         bulletParticleSystem.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
     }
+
+    [System.Obsolete]
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("reflect");
+        // Partikel kollidiert mit einem Collider (z.B. eine Wand)
+
+        ParticleCollisionEvent[] collisionEvents = new ParticleCollisionEvent[16];
+        int numCollisionEvents = bulletParticleSystem.GetCollisionEvents(other, collisionEvents);
+
+        for (int i = 0; i < numCollisionEvents; i++)
+        {
+            // Erhalte die Kontakt-Normalenrichtung
+            Vector3 contactNormal = collisionEvents[i].normal;
+            Debug.Log(contactNormal);
+
+            // Verwende die Kontakt-Normalenrichtung, um das Partikelverhalten anzupassen
+            // z.B. indem du den reflektierten Richtungsvektor für das Partikelsystem berechnest.
+        }
+    }
+
 
 }
