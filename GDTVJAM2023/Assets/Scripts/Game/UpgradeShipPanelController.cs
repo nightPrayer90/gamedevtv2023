a564197851e5eb5ffbee90b0e757f6ff420c07cc
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2023-09-13 23:38:18 +0200

    bugfixing

--- GDTVJAM2023/Assets/Scripts/Game/UpgradeShipPanelController.cs
+++ GDTVJAM2023/Assets/Scripts/Game/UpgradeShipPanelController.cs
@@ -9,16 +9,9 @@ public class UpgradeShipPanelController : MonoBehaviour
     private Vector3 localPositon;
 
     [HideInInspector] public List<Color> classColors;
-    //public List<Image> classPanels = new List<Image>();//
-
-    //public List<Image> selectedUpgradePanelList = new List<Image>();
-
 
     //Objects
     private GameManager gameManager;
-    //private UpgradeChooseList upgradeChooseList;
-    //private PlayerController playerController;
-    //private PlayerWeaponController playerWeaponController;
 
     public Image ShipPanel;
     public List<Sprite> shipImages = new List<Sprite>();
@@ -30,18 +23,9 @@ public class UpgradeShipPanelController : MonoBehaviour
 
         // set Objects
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
-        //upgradeChooseList = gameManager.GetComponent<UpgradeChooseList>();
-        //classColors = new List<Color>(gameManager.globalClassColor);
     }
     private void OnEnable()
     {
-        // set Objects
-        //playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
-        //playerWeaponController = GameObject.FindWithTag("Player").GetComponent<PlayerWeaponController>();
-
-        // Update content
-        //UpdateValuePanel();
-
         // FadeIn Tween
         transform.position = localPositon;
         transform.DOPunchScale(new Vector3(0.1f, 0.1f, 0.1f), 0.6f, 8, 1).SetUpdate(true);
@@ -68,24 +52,4 @@ public class UpgradeShipPanelController : MonoBehaviour
     {
         transform.DOLocalMoveY(-600, 0.5f, true).SetUpdate(true).SetEase(Ease.InQuart);
     }
-
-    // Update Values in Panel 4
-    public void UpdateValuePanel()
-    {
-        // update main weapon valuesText
-
-        Color defaultColor = new Color(0.2f, 0.2f, 0.2f, 0.5f);
-
-        // update class colors
-        /*if (upgradeChooseList.mcBulletLvl > 0) { classPanels[0].color = classColors[0]; } else { classPanels[0].color = defaultColor; }
-        if (upgradeChooseList.mcExplosionLvl > 0) { classPanels[1].color = classColors[1]; } else { classPanels[1].color = defaultColor; }
-        if (upgradeChooseList.mcLaserLvl > 0) { classPanels[2].color = classColors[2]; } else { classPanels[2].color = defaultColor; }
-        if (upgradeChooseList.mcSupportLvl > 0) { classPanels[3].color = classColors[3]; } else { classPanels[3].color = defaultColor; }
-
-        if (upgradeChooseList.scSwarmLvl > 0) { classPanels[4].color = classColors[4]; } else { classPanels[4].color = defaultColor; }
-        if (upgradeChooseList.scDefenceLvl > 0) { classPanels[5].color = classColors[5]; } else { classPanels[5].color = defaultColor; }
-        if (upgradeChooseList.scTargetingLvl > 0) { classPanels[6].color = classColors[6]; } else { classPanels[6].color = defaultColor; }
-        if (upgradeChooseList.scDirectionLvl > 0) { classPanels[7].color = classColors[7]; } else { classPanels[7].color = defaultColor; }*/
-    }
-
 }
