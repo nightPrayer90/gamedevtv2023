b0e7d490834c3d2a17dfc08a5857ef00fb8aafc7
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2024-01-15 18:18:42 +0100

    New Player System

--- GDTVJAM2023/Assets/Scripts/Teststuff/NewDirectionControlEngine.cs
+++ GDTVJAM2023/Assets/Scripts/Teststuff/NewDirectionControlEngine.cs
@@ -4,29 +4,83 @@ using UnityEngine;
 
 public class NewDirectionControlEngine : MonoBehaviour
 {
-    private NewPlayerController playerController;
-    [SerializeField] private ParticleSystem ps_engine;
-    private Rigidbody playerRigidbody;
-    public float torqueForce = 1f;
-
     private enum Side
     {
         left,
         right
     }
 
+    private NewPlayerController playerController;
+    [SerializeField] private ParticleSystem ps_engine;
+    private Rigidbody playerRigidbody;
+    public float torqueForce = 1f;
+    private Vector3 engineUPPos;
 
     [SerializeField]
     private Side engine_side;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<NewPlayerController>();
         playerRigidbody = playerController.GetComponent<Rigidbody>();
+        engineUPPos = transform.up;
 
         // Chance the center of Mass
         //playerRigidbody.centerOfMass = playerRigidbody.centerOfMass + new Vector3(transform.localPosition.x/2, 0f, transform.localPosition.z/2);
+
+        float z_Position = Mathf.Sign(playerRigidbody.centerOfMass.z + transform.localPosition.z);
+        float x_Position = Mathf.Sign(playerRigidbody.centerOfMass.x + transform.localPosition.x);
+        int engineRotation = Mathf.RoundToInt(transform.localRotation.eulerAngles.y);
+
+        switch (engineRotation)
+        {
+            case 0:
+                if (x_Position == -1)
+                {
+                    engineUPPos = -transform.up;
+                }
+                if (x_Position == 1)
+                {
+                    engineUPPos = transform.up;
+                }
+                break;
+
+            case 90:
+                if (z_Position == -1)
+                {
+                    engineUPPos = transform.up;
+                }
+                if (z_Position == 1)
+                {
+                    engineUPPos = -transform.up;
+                }
+                break;
+
+            case 180:
+                if (x_Position == -1)
+                {
+                    engineUPPos = transform.up;
+                }
+                if (x_Position == 1)
+                {
+                    engineUPPos = -transform.up;
+                }
+                break;
+            case 270:
+                if (z_Position == -1)
+                {
+                    engineUPPos = -transform.up;
+                }
+                if (z_Position == 1)
+                {
+                    engineUPPos = transform.up;
+                }
+                break;
+        }
     }
 
     // Update is called once per frame
@@ -34,103 +88,11 @@ public class NewDirectionControlEngine : MonoBehaviour
     {
         if (playerController.horizontalInput > 0.1)
         {
-            if (engine_side == Side.left)
+            if (engine_side == Side.right)
             {
-                // Berechne das Drehmoment basierend auf der seitlichen Eingabe
-                //Vector3 torque = new Vector3(0f, playerController.horizontalInput, 0f) * torqueForce;
-                // Holen Sie sich die lokale Up-Achse des Triebwerks
-                
-                // to do - muss nicht zur laufzeit passieren
-                float z_Position = Mathf.Sign(playerRigidbody.centerOfMass.z + transform.localPosition.z);
-                float x_Position = Mathf.Sign(playerRigidbody.centerOfMass.x + transform.localPosition.x);
-                Vector3 engineUPPos = transform.up;
-                int engineRotation = Mathf.RoundToInt(transform.localRotation.eulerAngles.y);
-
-                switch (engineRotation)
-                {
-                    case 0:
-                        //Unterhalb des schwerpunktes
-                        if (x_Position == -1)
-                        {
-                            //Vector3 engineUp = (transform.localRotation.y - 0.5f) * transform.up;
-                            engineUPPos = -transform.up;
-                        }
-                        if (x_Position == 1)
-                        {
-                            //Vector3 engineUp = (transform.localRotation.y - 0.5f) * transform.up;
-                            engineUPPos = transform.up;
-                        }
-                        break;
-
-                    case 90:
-                        //Unterhalb des schwerpunktes
-                        if (z_Position == -1)
-                        {
-                            //Vector3 engineUp = (transform.localRotation.y - 0.5f) * transform.up;
-                            engineUPPos = transform.up;
-                        }
-                        if (z_Position == 1)
-                        {
-                            //Vector3 engineUp = (transform.localRotation.y - 0.5f) * transform.up;
-                            engineUPPos = -transform.up;
-                        }
-                        break;
-
-                    case 180:
-                        //Unterhalb des schwerpunktes
-                        if (x_Position == -1)
-                        {
-                            //Vector3 engineUp = (transform.localRotation.y - 0.5f) * transform.up;
-                            engineUPPos = transform.up;
-                        }
-                        if (x_Position == 1)
-                        {
-                            //Vector3 engineUp = (transform.localRotation.y - 0.5f) * transform.up;
-                            engineUPPos = -transform.up;
-                        }
-                        break;
-                    case 270:
-                        //Unterhalb des schwerpunktes
-                        if (z_Position == -1)
-                        {
-                            //Vector3 engineUp = (transform.localRotation.y - 0.5f) * transform.up;
-                            engineUPPos = -transform.up;
-                        }
-                        if (z_Position == 1)
-                        {
-                            //Vector3 engineUp = (transform.localRotation.y - 0.5f) * transform.up;
-                            engineUPPos = transform.up;
-                        }
-                        break;
-
-                }
-
-
-
-
-
-
-
-                // Berechne die Drehkraft basierend auf der seitlichen Eingabe und der Kraftrichtung
                 Vector3 torque = engineUPPos * playerController.horizontalInput * torqueForce;
 
-              
-                //Debug.Log("engineUP " + engineUp);
-                Debug.Log("engineUPpos " + engineUPPos);
-
-                Debug.Log("z_Position " + z_Position + " / x_Position " + x_Position);
-       
-
-
-                // Debug.Log("localRotation " + transform.localRotation.y);*/
-
-
-                // Berechne die Drehkraft basierend auf der seitlichen Eingabe und der Up-Achse des Triebwerks
-                //Vector3 torque = engineUp * playerController.horizontalInput * torqueForce;
-
-                // Wende die Drehkraft auf das Rigidbody an
                 playerRigidbody.AddTorque(torque);
-
 
                 ps_engine.Emit(1);
             }
@@ -138,20 +100,11 @@ public class NewDirectionControlEngine : MonoBehaviour
 
         if (playerController.horizontalInput < -0.1)
         {
-            if (engine_side == Side.right)
+            if (engine_side == Side.left)
             {
-                // Berechne das Drehmoment basierend auf der seitlichen Eingabe
-                //Vector3 torque = new Vector3(0f, playerController.horizontalInput, 0f) * torqueForce;
+                Vector3 torque = engineUPPos * -playerController.horizontalInput * torqueForce;
 
-                // Holen Sie sich die lokale Up-Achse des Triebwerks
-                Vector3 engineUp = transform.up;
-
-                // Berechne die Drehkraft basierend auf der seitlichen Eingabe und der Up-Achse des Triebwerks
-                Vector3 torque = engineUp * playerController.horizontalInput * torqueForce;
-
-                // Wende die Drehkraft auf das Rigidbody an
                 playerRigidbody.AddTorque(torque);
-
 
                 ps_engine.Emit(1);
             }
